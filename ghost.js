9db6d763e9ed08114d9f426cbc5d6844a378195e
Eric Caleb <calebme@mail.com>
2016-09-29 16:54:54 +0100

    Added ghost server

---
+++ ghost.js
@@ -0,0 +1,46 @@
+var http = require("http");
+var fs  = require("fs");
+var path = require("path");
+
+http.createServer(function(req, res){
+    console.log(`${req.method} request for ${req.url}`);
+
+    if (req.url === "/"){
+        fs.readFile("./index.html", "UTF-8", function(err, html){
+        res.writeHead(200 ,{"Content-Type":"text/html"} );
+        res.end(html);
+
+        });
+     } else if (req.url.match(/.css$/)){
+
+        var cssPath = path.join(__dirname , '/' , req.url);
+        var fileStream = fs.createReadStream(cssPath , "UTF-8");
+
+        res.writeHead(200 , {"Content-Type":"text/css"});
+
+        fileStream.pipe(res);
+     }else if (req.url.match(/.js$/)){
+
+        var cssPath = path.join(__dirname , '/' , req.url);
+        var fileStream = fs.createReadStream(cssPath , "UTF-8");
+
+        res.writeHead(200 , {"Content-Type":"text/javascript"});
+
+        fileStream.pipe(res);
+
+     } else if (req.url.match(/.jpg/ || /.png/  )) {
+
+        var imgPath = path.join(__dirname , "/", req.url);
+        var imgStream =  fs.createReadStream(imgPath);
+
+        res.writeHead(200, {"Content-Type":"image/jpeg"} || {"Content-Type":"image/png"})
+        imgStream.pipe(res);
+    
+     } else {
+        res.writeHead(404 , {"Content-Type":"text/plain"});
+        res.end("404 Ghost File");
+    }
+
+}).listen(7000); 
+
+console.log("ghost server running on port 7000");
