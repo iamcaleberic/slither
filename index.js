50cb54e5e91a64f77346f44d6ec40d875a35eac1
Eric Caleb <calebme@mail.com>
2016-09-29 15:44:15 +0100

    Added render function

--- index.js
+++ index.js
@@ -1,93 +1,108 @@
 "use strict";
 
 var canvas = document.querySelector("#canvas");
-	var ctx = canvas.getContext('2d');
-	BOARD_SIZE =  25
+var ctx = canvas.getContext('2d');
+BOARD_SIZE =  25
 
-	canvas.width =  window.innerWidth - 20
-	canvas.height = window.innerHeight - 20
-	ctx.font = "20px veranda"
+canvas.width =  window.innerWidth - 20
+canvas.height = window.innerHeight - 20
+ctx.font = "20px veranda"
 
-	var cw = canvas.width / BOARD_SIZE,
-			 		ch = canvas.height/ BOARD_SIZE
-
-
-	var snake, food , score , high_score = 0
+var cw = canvas.width / BOARD_SIZE,
+		 		ch = canvas.height/ BOARD_SIZE
 
 
-	function random(min , max){
-		return Math.floor(Math.random() * (max - min) + min)
+var snake, food , score , high_score = 0
+
+
+function random(min , max){
+	return Math.floor(Math.random() * (max - min) + min)
+}
+
+
+function reset(){
+	score = 0
+
+	snake = {
+		direction: 'right',
+		body: [
+
+			{x: 3 , y: 0},
+			{x: 2 , y: 0},
+			{x: 1 , y: 0},
+			{x: 0 , y: 0}
+		]
 	}
-	
-	
-	function reset(){
-		score = 0
 
-		snake = {
-			direction: 'right',
-			body: [
+	food = {
+		x: random(0, BOARD_SIZE),
+		y: random(0, BOARD_SIZE),
+	}
+}
 
-				{x: 3 , y: 0},
-				{x: 2 , y: 0},
-				{x: 1 , y: 0},
-				{x: 0 , y: 0}
-			]
-		}
+function tick(){
+	snake.head = {
+		x: snake.body[0].x,
+		y: snake.body[0].y
+	}
+
+
+	if (snake,direction ==  "right")  snake.head.x++
+	else if (snake,direction ==  "left")  snake.head.x--
+	else if (snake,direction ==  "down")  snake.head.y++
+	else if (snake,direction ==  "up")  snake.head.y--
+
+
+	// food check
+
+	if (snake.head.x == food.x && snake.head.y == food.y){
+		score++
+
+		if(score > high_score) high_score = score
 
 		food = {
-			x: random(0, BOARD_SIZE),
+			x: random(0 , BOARD_SIZE),
 			y: random(0, BOARD_SIZE),
 		}
+	}else{
+		snake.body.pop()
 	}
 
-	function tick(){
-		snake.head = {
-			x: snake.body[0].x,
-			y: snake.body[0].y
-		}
+	// if nigga collided
 
-
-		if (snake,direction ==  "right")  snake.head.x++
-		else if (snake,direction ==  "left")  snake.head.x--
-		else if (snake,direction ==  "down")  snake.head.y++
-		else if (snake,direction ==  "up")  snake.head.y--
-
-
-		// food check
-
-		if (snake.head.x == food.x && snake.head.y == food.y){
-			score++
-
-			if(score > high_score) high_score = score
-
-			food = {
-				x: random(0 , BOARD_SIZE),
-				y: random(0, BOARD_SIZE),
-			}
-		}else{
-			snake.body.pop()
-		}
-
-		// if nigga collided
-
-		if (collision()){
-			reset()
-		}else{
-			snake.body.unshift(snake.head)
-		}
-
-		render()
-		setTimeout(tick , 100 - score * 5)
+	if (collision()){
+		reset()
+	}else{
+		snake.body.unshift(snake.head)
 	}
 
-	function collision() {
-		return(
-			snake.head.x < 0 || 
-			snake.head.x >|| BOARD_SIZE ||
-			snake.head.y < 0 ||
-			snake.body.some(function (cell) {
-				return(cell.x ==  snake.head.x && cell.y == snake.head.y)
-			})
+	render()
+	setTimeout(tick , 100 - score * 5)
+}
 
-			)
-	}
+function collision() {
+	return(
+		snake.head.x < 0 || 
+		snake.head.x >|| BOARD_SIZE ||
+		snake.head.y < 0 ||
+		snake.body.some(function (cell) {
+			return(cell.x ==  snake.head.x && cell.y == snake.head.y)
+		})
+
+		)
+}
+
+function render() {
+	ctx.fillStyle = "black"
+	ctx.fillRect(0, 0 , canvas.width , canvas.height)
+
+	snake.body.forEach(function(cell){
+		paint_cell(cell, 'blue')
+	})
+
+	paint_cell(food, "red")
+
+	ctx.fillStyle = "white"
+	ctx.fillText("Score:" + score , 10, 20)
+	ctx.fillText("High Score:" + high_score, 10, 45 )
+}
