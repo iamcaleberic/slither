81c14ff3883899ebec3dea370c3c9d905d5aa2b0
Eric Caleb <calebme@mail.com>
2016-09-29 15:21:11 +0100

    Added index js file

---
+++ index.js
@@ -0,0 +1,11 @@
+"use strict";
+
+var canvas = document.querySelector("#canvas");
+	var ctx = canvas.getContext('2d');
+	BOARD_SIZE =  25
+
+	canvas.width =  window.innerWidth - 20
+	canvas.height = window.innerHeight - 20
+	
+	
+	
